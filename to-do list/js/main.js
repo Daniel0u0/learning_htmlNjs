0eb1406a3c399e0167ca27bc6dbd92e6ecd354ae
Daniel0u0 <130910330+Daniel0u0@users.noreply.github.com>
2024-02-12 15:49:10 +0800

    update todo list

--- to-do list/js/main.js
+++ to-do list/js/main.js
@@ -3,27 +3,28 @@ const todoForm = document.getElementById('todoForm')
 	const todoInput = document.getElementById('todoInput')
 	
 function createToDoItem(text){
-	const li = document.createElement('li')
-	li.className = "todo-item"
-	li.innerHTML =
-	
-					<input type="checkbox" class="todo-item-checkbox">
-					<span class="todo-item-text>${text}</span>
-					<button class="todo-item-delete">Delete</button>
-					
-	return li
-}
+        const li = document.createElement('li')
+        li.className = "todo-item"
+        li.innerHTML =
+                        `
+                        <input type="checkbox" class="todo-item-checkbox">
+                        <span class="todo-item-text">${text}</span>
+                        <button class="todo-item-delete">Delete</button>
+                        `
+    return li
+    }
 
-	todoForm.onclick = function (event){
+
+	todoForm.onclick = function (event) {
 		event.preventDefault()
-		const todoText = todoInput.value
+		const  todoText = todoInput.value
 		if(!todoText) return
-		const li = createToDoItem(todoInput.value.trim())
+		const li =createToDoItem(todoInput.value.trim())
 		todoList.appendChild(li)
 		todoInput.value=""
 	}
 
-	todolist.onclick = function(event){
-		if(event.target.className==="todo-item-delete" && confirm('Are you sure you want to delete it?'))
-			event.targetr.parentNode.remove()
-	}
+    todoList.onclick = function (event){
+        if(event.target.className==="todo-item-delete" && confirm('Are you sure you want to delete it?'))
+            event.target.parentNode.remove()
+    }
